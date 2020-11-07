e760f56374fe93b0a71b068c67a9d3873b9101c8
YujinKim-Sciedu <sandwich17yj@likelion.org>
2020-11-07 22:28:37 +0900

    게임시작화면

---
+++ Assets/Scripts/JH_Scene/startGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    public void StartPrologue()
+    {
+        SceneManager.LoadScene("Plologue");
+    }
+
+    public void SkipPlologueScene()
+    {
+        SceneManager.LoadScene("JH_Scene");
+    }
+}
+
